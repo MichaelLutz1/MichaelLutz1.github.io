fb9525dcc383120006fd11f7c5b17c2939d3a802
Michael Lutz <mikelutz2400@gmail.com>
2023-01-21 12:43:29 -0500

    intro statement

---
+++ script.js
@@ -0,0 +1,31 @@
+const hellos = [
+  "Hola",
+  "Bonjour",
+  "Namaste",
+  "Nǐn hǎo",
+  "Guten tag",
+  "Ciao",
+  "Anyoung",
+  "Olá",
+  "Privet",
+  "G'day",
+  "Shalom",
+  "Hello",
+];
+
+const hello = document.getElementById("hello");
+let i = 0;
+setInterval(flipIn, 5000);
+function flipIn() {
+  i++;
+  if (i === hellos.length) {
+    i = 0;
+  }
+  hello.classList.add("flip-out");
+  setTimeout(() => {
+    hello.innerHTML = hellos[i];
+    hello.classList.add("flip-in");
+    hello.classList.remove("flip-out");
+  }, 500);
+  hello.classList.remove("flip-in");
+}
