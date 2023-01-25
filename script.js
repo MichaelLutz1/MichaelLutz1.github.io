98b229f18ed4bb27b6b9d16b944b10bda23edecc
Michael Lutz <mikelutz2400@gmail.com>
2023-01-24 22:04:26 -0500

    dark mode slider

--- script.js
+++ script.js
@@ -29,3 +29,15 @@ function flipIn() {
   }, 500);
   hello.classList.remove("flip-in");
 }
+
+const toggleSwitch = document.querySelector("#darkmode-checkbox");
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme);
